beb4b93f33d3a94452742a85d4a6a1ff355b4ba3
Adam Goldberg <112718049+agold95@user.noreply.github.com>
2023-05-22 12:42:01 -0400

    GameController setup

--- main.js
+++ main.js
@@ -36,3 +36,67 @@ const Gameboard = (() => {
         resetBoard
     }
 })();
+
+const GameController = (() => {
+    const playerX = Player('X');
+    const playerO = Player('O');
+    let round = 1;
+    let isOver = false;
+
+    // double check this one
+    const getPlayerSign = () => {
+        if (round % 2 === 1) {
+        return playerX.getMarker();
+        } else {
+        return playerO.getMarker();
+        }
+    }
+
+    const checkWinner = (markerIndex) => {
+        const winCondition = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 5],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6]
+        ];
+
+        return winCondition
+        .filter((combo) => combo.includes(markerIndex))
+        .some((possiblecombo) => possiblecombo.every(
+            (index) => Gameboard.getMarker(index) === getPlayerSign())
+        );
+    };
+
+    const getIsOver = () => {
+     return isOver;   
+    };
+
+    const reset = () => {
+        round = 1;
+        isOver = false;
+    };
+
+    const playRound = (markerIndex) => {
+        Gameboard.setMarker(markerIndex, getPlayerSign());
+        if (checkWinner(markerIndex)) {
+            DisplayController.setResult(getPlayerSign());
+            isOver = true;
+            return;
+        }
+        if (round === 9) {
+            DisplayController.setResult("Tie!");
+            isOver = true;
+            return;
+        }
+        round++;
+        DisplayController.setMessage(`${getPlayerSign()}'s turn`);
+    };
+})();
+
+const DisplayController = (() => {
+
+})
